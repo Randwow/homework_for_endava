f1c7b9b5f350a289bb33bdd833babbb1d55bf08a
Никита <43860428+Randwow@users.noreply.github.com>
2020-10-03 21:55:38 +0300

    finished work

--- User/src/com/company/Main.java
+++ User/src/com/company/Main.java
@@ -1,9 +1,35 @@
 package com.company;
 import java.util.ArrayList;
 import java.util.*;
-public class Main {
+import com.company.UserList.*;
+import com.company.User.*;
+import com.company.*;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        UserList.addUser("Nichita", "Macheev", 18,"nichita.macheev@gmail.com","New","3.10.2020" );
+        UserList.addUser("Xenia", "Miron", 34,"xenia.miron@gmail.com","New","2.10.2020" );
+        UserList.addUser("Alina", "Malkova", 21,"alina.malkova@gmail.com","INACTIVE","3.10.2019" );
+        UserList.addUser("Alex", "Petrov", 45,"alex.petrov@gmail.com","Active","3.10.2009" );
+        System.out.println(UserList.getUser("Nichita","Macheev"));
+        System.out.println(UserList.getUser("Xenia","Miron"));
+        System.out.println(UserList.getUser("Alina","Malkova"));
+        System.out.println(UserList.getUser("Alex","Petrov"));
+        System.out.println("\nCall the method that changes the status\n");
+        UserList.changeStatus();
+        System.out.println(UserList.getUser("Nichita","Macheev"));
+        System.out.println(UserList.getUser("Xenia","Miron"));
+        System.out.println(UserList.getUser("Alina","Malkova"));
+        System.out.println(UserList.getUser("Alex","Petrov"));
+        System.out.println("\nCall the method that changes the status INACTIVE on Blocked\n");
+        UserList.inactiveMethod();
+        System.out.println(UserList.getUser("Nichita","Macheev"));
+        System.out.println(UserList.getUser("Xenia","Miron"));
+        System.out.println(UserList.getUser("Alina","Malkova"));
+        System.out.println(UserList.getUser("Alex","Petrov"));
+        UserList.delUser("Macheev");
+        UserList.delUser("Miron");
+        UserList.delUser("Malkova");
+        UserList.delUser("Petrov");
     }
 }
--- User/src/com/company/UserList.java
+++ User/src/com/company/UserList.java
@@ -2,5 +2,51 @@ package com.company;
 import java.util.*;
 public class UserList {
     private static List<User> myList = new ArrayList<>();
-
+    public static User getUser(String firstname, String lastname) {
+        for(User u : myList)
+            if(u.getFirstname().equals(firstname) && u.getLastname().equals(lastname))
+                return u;
+        return null;
+    }
+    public static void addUser(String firstname, String lastname, int age, String email, String status, String timestamp){
+        if(getUser(firstname, lastname) == null)
+            myList.add(new User(firstname, lastname, age, email, status, timestamp));
+        else
+            System.out.println("Такой пользователь уже существует!");
+    }
+    public static void delUser(String lastname) {
+        if(!myList.isEmpty()) {
+            Iterator<User> iter = myList.iterator();
+            while(iter.hasNext()) {
+                User u = iter.next();
+                if(u.getLastname().equals(lastname))
+                    iter.remove();
+            }
+        }
+        else
+            System.out.println("Список пользователей пуст!");
+    }
+    public static void changeStatus(){
+        if(myList.isEmpty()){
+            System.out.println("Список пользователей пуст!");
+        }else{
+            Iterator<User> iter = myList.iterator();
+            while(iter.hasNext()) {
+                User u = iter.next();
+                if(u.getTimestamp().equals("3.10.2020")){ u.setStatus("New");}
+                if((u.getTimestamp().compareTo("3.10.2020") < 0)&& u.getStatus().equals("New")){ u.setStatus("Active ");}
+            }
+        }
+    }
+    public static void inactiveMethod(){
+        if(myList.isEmpty()){
+            System.out.println("Список пользователей пуст!");
+        }else{
+            Iterator<User> iter = myList.iterator();
+            while(iter.hasNext()) {
+                User u = iter.next();
+                if((u.getTimestamp().compareTo("3.9.2020") < 0) && u.getStatus().equals("INACTIVE")){ u.setStatus("Blocked ");}
+            }
+        }
+    }
 }
